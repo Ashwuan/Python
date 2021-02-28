b587fd633fcf53e1e013047d950d19d366ee1b5f
Ashwuan K. Williams <ashwuanwill@gmail.com>
2021-02-27 20:57:20 -0500

    MadLibsGame

---
+++ MadLibsGame.py
@@ -0,0 +1,7 @@
+FavGame = input('Enter Favorite Video Game: ')
+Adjective = input ('Enter an adjective: ')
+Creator = input('Enter Video Game Creator: ')
+
+print(FavGame + ' is my favorite video game')
+print('it is such a/an' ,Adjective, 'game')
+print('This game was created by ' + Creator)
