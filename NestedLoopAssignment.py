f22a7534c2ae83a20a1af8ceb6000aca80f0e516
Ashwuan K. Williams <ashwuanwill@gmail.com>
2021-03-19 02:24:56 -0400

    Nested Loop Assignment

---
+++ NestedLoopAssignment.py
@@ -0,0 +1,11 @@
+#you have a list of fruits
+
+fruits = ["apple", "banana", "cherry"]
+
+#and you have a list of adjectives for the fruits:
+
+adj = ["red", "big", "tasty"]
+
+for x in adj:
+    for y in fruits:
+        print(x,y)
