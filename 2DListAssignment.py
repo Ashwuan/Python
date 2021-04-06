ab40df48dd69bfc8db6c459387af0fd1bbd07d60
Ashwuan K. Williams <ashwuanwill@gmail.com>
2021-04-06 13:23:14 -0400

    2DListAssignment

---
+++ 2DListAssignment.py
@@ -0,0 +1,16 @@
+GridInput = int(input("Enter the corresponding index for the column you would like printed (0,1,2): "))
+
+number_grid = [ 
+[1,2,3],
+[4,5,6],
+[7,8,9],
+[0]
+]
+
+for col in number_grid:
+    try:
+        print(col[GridInput])
+    except:
+        print("list index out of range")
+
+   
